8e9da4a6daabfd18cc82a61240217730762f8b97
S M Sabbir Ahmed <103526592+SM-Sabbir@users.noreply.github.com>
2024-08-03 18:20:37 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ContactController::class, 'index'])->name('contacts.index');
 Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
 Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
 Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
